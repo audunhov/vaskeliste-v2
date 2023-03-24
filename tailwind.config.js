42586ccb69b650d693a274c231fac3734bebc15e
Audun Hammer Hovda <hammerhovda@gmail.com>
2023-03-25 00:54:52 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 module.exports = {
-    plugins: [
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/line-clamp'),
-      require('@tailwindcss/aspect-ratio'),
-    ]
-  }
+  content: [
+    "components/**/*.{vue,js,ts}",
+    "layouts/**/*.vue",
+    "pages/**/*.vue",
+    "composables/**/*.{js,ts}",
+    "plugins/**/*.{js,ts}",
+    "utils/**/*.{js,ts}",
+    "App.{js,ts,vue}",
+    "app.{js,ts,vue}",
+    "Error.{js,ts,vue}",
+    "error.{js,ts,vue",
+  ],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
