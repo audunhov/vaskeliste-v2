a6e03ff1e5c88c09224794452eda38ea7e05dbaa
Audun Hammer Hovda <hammerhovda@gmail.com>
2023-03-25 01:26:58 +0100

    65cd804

---
+++ _nuxt/index.90c1dddc.js
@@ -0,0 +1,1 @@
+import{a as e,i as t,o,b as r}from"./entry.7d7ca6c8.js";const l=e({__name:"index",setup(n){return t().push("/current"),(a,u)=>(o(),r("div",null,"Vaskeliste for kollektivet"))}});export{l as default};
