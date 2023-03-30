329f8b26ba928b7a51acc9921c834250a45a8494
Audun Hammer Hovda <hammerhovda@gmail.com>
2023-03-30 23:28:30 +0200

    fixed config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    app: {
-        baseURL: "/vaskeliste/"
-    },
     modules: [
         '@vite-pwa/nuxt',
         "@nuxtjs/tailwindcss",
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,3 @@
 <template>
     <div>Vaskeliste for kollektivet</div>
 </template>
-
-<script setup lang="ts">
-const router = useRouter()
-router.push("/current")
-</script>
